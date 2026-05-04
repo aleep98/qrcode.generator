cfb90941a4b7583125f11fdbd5de127da10f97b9
aleep98 <zero167@outlook.com>
2026-05-04 20:49:09 -0300

    Adiciona arquivos de configuração do Maven e estrutura inicial do projeto QR Code

---
+++ src/main/java/com/aleedev/qrcode/generator/controller/QrCodeController.java
@@ -0,0 +1,21 @@
+package com.aleedev.qrcode.generator.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aleedev.qrcode.generator.dto.QrCodeGenerateRequest;
+import com.aleedev.qrcode.generator.dto.QrCodeGenerateResponse;
+
+@RestController
+@RequestMapping("/qrcode")
+
+public class QrCodeController {
+
+    @PostMapping
+    public ResponseEntity<QrCodeGenerateResponse > generate(@RequestBody QrCodeGenerateRequest request) {
+        return null;
+}
+}
---
+++ src/main/java/com/aleedev/qrcode/generator/dto/QrCodeGenerateRequest.java
@@ -0,0 +1,5 @@
+package com.aleedev.qrcode.generator.dto;
+
+public record QrCodeGenerateRequest(String text) {
+
+}
