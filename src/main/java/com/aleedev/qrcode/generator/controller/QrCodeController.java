c3e9eba9223d44859b86fd15b272ddafa3f7e134
aleep98 <zero167@outlook.com>
2026-05-07 06:34:08 -0300

    Implementa serviço de geração e upload de QR Code, atualiza controlador e configurações do S3

--- src/main/java/com/aleedev/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/aleedev/qrcode/generator/controller/QrCodeController.java
@@ -8,14 +8,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.aleedev.qrcode.generator.dto.QrCodeGenerateRequest;
 import com.aleedev.qrcode.generator.dto.QrCodeGenerateResponse;
+import com.aleedev.qrcode.generator.service.QrCodeGeneratorService;
 
 @RestController
 @RequestMapping("/qrcode")
 
 public class QrCodeController {
 
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+    public QrCodeController(QrCodeGeneratorService qrCodeService) {
+        this.qrCodeGeneratorService = qrCodeService;
+    }
+
     @PostMapping
     public ResponseEntity<QrCodeGenerateResponse > generate(@RequestBody QrCodeGenerateRequest request) {
-        return null;
+try {
+    QrCodeGenerateResponse response = this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
+    return ResponseEntity.ok(response);
+
+} catch (Exception e) {
+    return ResponseEntity.internalServerError().build();
 }
+    }
 }
